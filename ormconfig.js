1f9c0561a0d5a58019e31e5ce2c1f79c3ab5729b
Felipe Nunes <luis.felipe.nb@hotmail.com>
2022-02-15 15:54:57 -0300

    feat: adding book entity

--- ormconfig.js
+++ ormconfig.js
@@ -1,36 +1,36 @@
 const env = {
-	type: "postgres",
-	host: process.env.DATABASE_HOST || "localhost",
-	url: process.env.DATABASE_URL || undefined,
-	ssl:
-		process.env.NODE_ENV === "production"
-			? { rejectUnauthorized: false }
-			: false,
-	port: 5432,
-	username: "test",
-	password: "test",
-	database: "test",
-	synchronize: true,
-	logging: true,
-	entities:
-		process.env.NODE_ENV === "production"
-			? ["dist/src/entities/**/*.js"]
-			: ["src/entities/**/*.ts"],
-	migrations:
-		process.env.NODE_ENV === "production"
-			? ["dist/src/database/migration/**/*.js"]
-			: ["src/database/migration/**/*.ts"],
-	cli: {
-		entitiesDir: "src/entities",
-		migrationsDir: "src/database/migration",
-	},
+  type: "postgres",
+  host: process.env.DATABASE_HOST || "localhost",
+  url: process.env.DATABASE_URL || undefined,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
+  port: 5431,
+  username: "test",
+  password: "test",
+  database: "test",
+  synchronize: true,
+  logging: true,
+  entities:
+    process.env.NODE_ENV === "production"
+      ? ["dist/src/entities/**/*.js"]
+      : ["src/entities/**/*.ts"],
+  migrations:
+    process.env.NODE_ENV === "production"
+      ? ["dist/src/database/migration/**/*.js"]
+      : ["src/database/migration/**/*.ts"],
+  cli: {
+    entitiesDir: "src/entities",
+    migrationsDir: "src/database/migration",
+  },
 };
 
 const testEnv = {
-   type: "sqlite",
-   database: ":memory:",
-   entities: ["src/entities/**/*.ts"],
-   synchronize: true
-}
+  type: "sqlite",
+  database: ":memory:",
+  entities: ["src/entities/**/*.ts"],
+  synchronize: true,
+};
 
 module.exports = process.env.NODE_ENV === "test" ? testEnv : env;
