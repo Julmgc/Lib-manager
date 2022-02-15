dc892472caa87a8e1e813c4b4591b9e0a5479a3f
Divinity <systemdivinity@gmail.com>
2022-02-15 09:25:15 -0300

    feat/userBooks

---
+++ src/entities/userBooksEntity.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, ManyToOne, Column } from "typeorm";
+
+@Entity("user_books")
+export default class User_Books {
+  @PrimaryGeneratedColumn("increment")
+  id!: number;
+
+  @ManyToOne("")
+  user_id!: number;
+
+  @ManyToOne("")
+  book_id!: number;
+
+  @Column("date", { default: new Date() })
+  checkout_date!: Date;
+
+  @Column("date", { default: new Date() })
+  return_date!: Date;
+
+  @Column("date", { nullable: true })
+  renew_date!: Date;
+}
