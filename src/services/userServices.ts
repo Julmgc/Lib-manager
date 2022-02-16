c66c989a292193d51af946e71bf8d999144f8bfe
Divinity <systemdivinity@gmail.com>
2022-02-16 11:50:26 -0300

    feat/signup

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,8 +1,25 @@
 import { getCustomRepository } from "typeorm";
 import UserRepository from "../repositories/userRepository";
+import { userInterface } from "../types";
+import User from "../entities/userEntity";
 
 export class UserServices {
-	static userRepository = () => {
-		return getCustomRepository(UserRepository);
-	};
+  static userRepository = () => {
+    return getCustomRepository(UserRepository);
+  };
+  public static async createUser(userData: userInterface): Promise<User> {
+    const userRepo = this.userRepository();
+
+    const user = userRepo.create({
+      name: userData.name,
+      email: userData.email,
+      password: userData.password,
+      authorized: userData.authorized,
+      isAdm: userData.isAdm,
+      address: userData.address,
+      cpf: userData.cpf,
+    });
+    await userRepo.save(user);
+    return user;
+  }
 }
