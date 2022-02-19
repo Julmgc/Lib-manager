81641b5a4e198e46e63485fc219268f43ccff9f6
André Luiz S. Vilarinho <andluizv@msn.com>
2022-02-18 23:44:46 -0300

    feat: renew route

--- src/services/userBooksServices.ts
+++ src/services/userBooksServices.ts
@@ -1,97 +1,101 @@
 import { getCustomRepository } from "typeorm";
 import UserBooksRepository from "../repositories/userBooksRepository";
-import UserRepository from "../repositories/userRepository";
 import BookRepository from "../repositories/bookRepository";
+import { BookServices } from "./bookServices";
+import { ApiError } from "../utils/errors";
+import { UserServices } from "./userServices";
+
 export class UserBooksServices {
-  static userBooksRepository = () => {
-    return getCustomRepository(UserBooksRepository);
-  };
+	static userBooksRepository = () => {
+		return getCustomRepository(UserBooksRepository);
+	};
 
-  static async loanBook(data: any, bookId: any) {
-    const userRepository = getCustomRepository(UserRepository);
-    const userBooksRepository = getCustomRepository(UserBooksRepository);
+	static async loanBook(data: any, bookId: any) {
+		const userBooksRepository = this.userBooksRepository();
 
-    const user = await userRepository.findOne({ email: data.userEmail });
+		const user = await UserServices.getByEmail(data.userEmail);
+		if (!user) {
+			throw new ApiError("User not found!", 404);
+		}
 
-    if (!user) {
-      return "User not found";
-    }
+		const bookRepository = getCustomRepository(BookRepository);
+		const book = await BookServices.findOneBook(bookId.bookId);
 
-    const bookRepository = getCustomRepository(BookRepository);
-    const book = await bookRepository.findOne({ where: { id: bookId.bookId } });
-    if (!book) {
-      return "Book not found";
-    }
+		const loanedBook = await userBooksRepository.findOne({
+			where: { book: bookId.bookId, user: user.id, returned: false },
+		});
+		if (loanedBook) {
+			throw new ApiError("This book was already loaned", 400);
+		}
 
-    const loanedBook = await userBooksRepository.findOne({
-      where: { book: bookId.bookId, user: user.id, returned: false },
-    });
-    if (loanedBook) {
-      return "This book was already loaned";
-    }
+		const countUserBook = await userBooksRepository.findAndCount({
+			where: { user: user.id },
+		});
 
-    var dt = new Date();
-    const renewDate = dt.setDate(dt.getDate() + 15);
-    const renewDateFormat = dt.toISOString().substring(0, 10);
+		if (countUserBook[1] >= 6) {
+			throw new ApiError("User already has 6 books loaned!", 400);
+		}
+		const userBook = userBooksRepository.create({
+			user: user,
+			book: book,
+			returned: false,
+		});
 
-    const countUserBook = await userBooksRepository.findAndCount({
-      where: { user: user.id },
-    });
+		await userBooksRepository.save(userBook);
 
-    if (countUserBook[1] >= 6) {
-      return "User already has 6 books loaned.";
-    }
-    const userBook = await userBooksRepository.create({
-      user: user,
-      book: book,
-      return_date: renewDateFormat,
-      returned: false,
-    });
+		book.loaned = true;
+		await bookRepository.save(book);
 
-    await userBooksRepository.save(userBook);
+		return await userBooksRepository.findOne({
+			where: { user: user, book: book, returned: false },
+		});
+	}
 
-    const bookLoaned = await bookRepository.findOne({
-      where: { id: bookId.bookId },
-    });
-    if (!bookLoaned) {
-      return "Book not found";
-    }
-    const bookData = { loaned: true };
-    await bookRepository.update(bookLoaned.id, bookData);
+	static async returnBook(bookId: string) {
+		const bookRepository = BookServices.bookRepository();
 
-    return await userBooksRepository.findOne({
-      where: { user: user, book: book, returned: false },
-    });
-  }
+		const book = await BookServices.findOneBook(bookId);
 
-  static async returnBook(bookId: string) {
-    const bookRepository = getCustomRepository(BookRepository);
+		const userBooksRepository = this.userBooksRepository();
 
-    const book = await bookRepository.findOne({ where: { id: bookId } });
-    if (!book) {
-      return "Book not found";
-    }
+		const userBook = await userBooksRepository.findOne({
+			where: { book: book, returned: false },
+		});
 
-    const userBooksRepository = getCustomRepository(UserBooksRepository);
+		if (!userBook) {
+			throw new ApiError("This book is not loaned!", 400);
+		}
 
-    const userBook = await userBooksRepository.findOne({
-      where: { book: book, returned: false },
-    });
+		const returnDate = new Date().toISOString();
 
-    if (!userBook) {
-      return "Book is not loaned";
-    }
+		const returneBook = { returned: true, return_date: returnDate };
 
-    var dt = new Date();
-    const returnDateFormat = dt.toISOString().substring(0, 10);
+		await userBooksRepository.update(userBook.id, returneBook);
 
-    const returneBook = { returned: true, return_date: returnDateFormat };
+		const bookData = { loaned: false };
+		await bookRepository.update(book.id, bookData);
 
-    await userBooksRepository.update(userBook.id, returneBook);
+		return await BookServices.findOneBook(bookId);
+	}
 
-    const bookData = { loaned: false };
-    await bookRepository.update(book.id, bookData);
+	static renew = async (bookId: string) => {
+		let book = BookServices.findOneBook(bookId);
+		const loaned = await this.userBooksRepository().findOne({
+			where: { book: { id: bookId } },
+		});
 
-    return await bookRepository.findOne({ where: { id: bookId } });
-  }
+		if (!loaned) {
+			throw new ApiError("This book is not on loan!", 400);
+		}
+		if (loaned.renewed) {
+			throw new ApiError("It's not possible to renew twice!", 400);
+		}
+
+		const oldDate = new Date(loaned.return_date);
+		loaned.return_date = new Date(oldDate.setDate(oldDate.getDate() + 7));
+		loaned.renewed = true;
+		await this.userBooksRepository().save(loaned);
+
+		return loaned;
+	};
 }
